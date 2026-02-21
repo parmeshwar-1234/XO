5c671e1fd673aaf268e93091f0d7ee700337c73e
parmeshwar-1234 <aparmeshwr@gmail.com>
2026-02-22 02:09:35 +0530

    Analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
